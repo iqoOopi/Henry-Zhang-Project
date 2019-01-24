130e2f298168a207c7f052b3b6a9bc4680c2a9c7
Haotian Zhang <778631@ACDM.DS.SAIT.CA>
2019-01-24 14:36:44 -0700

    Finished Day7

--- array-script.js
+++ array-script.js
@@ -1,60 +1,64 @@
-var imgArray=new Array();
-imgArray[0]="https://picsum.photos/500/300?random";
-imgArray[1]="https://picsum.photos/500/300?random";
-imgArray[2]="https://picsum.photos/500/300?random";
-imgArray[3]="https://picsum.photos/500/300?random";
-var imgDescri=new Array();
-imgDescri[0]="num 1 image";
-imgDescri[1]="num 2 image";
-imgDescri[2]="num 3 image";
-imgDescri[3]="num 4 image";
+var imgArray = new Array();
+imgArray[0] = "https://picsum.photos/500/300?random";
+imgArray[1] = "https://picsum.photos/500/300?random";
+imgArray[2] = "https://picsum.photos/500/300?random";
+imgArray[3] = "https://picsum.photos/500/300?random";
+imgArray[4] = "https://picsum.photos/500/300?random";
+var imgDescri = new Array();
+imgDescri[0] = "num 1 image";
+imgDescri[1] = "num 2 image";
+imgDescri[2] = "num 3 image";
+imgDescri[3] = "num 4 image";
+imgDescri[4] = "num 5 image";
 
-var position=document.getElementById("main-banner");
+var position = document.getElementById("main-banner");
 console.log(position);
 tbl = document.createElement("table");
 tbl.style.tableLayout = "fixed";
 tbl.style.width = '80%';
 tbl.style.textAlign = "center";
-tbl.style.marginLeft="auto";
-tbl.style.marginRight="auto";
-tr=tbl.insertRow();
+tbl.style.marginLeft = "auto";
+tbl.style.marginRight = "auto";
+tr = tbl.insertRow();
 // var td=tr.insertCell();
-th=document.createElement("th");
+th = document.createElement("th");
 th.appendChild(document.createTextNode("Image"));
 tr.appendChild(th);
-th=document.createElement("th");
+th = document.createElement("th");
 th.appendChild(document.createTextNode("Description"));
 tr.appendChild(th);
-for (var i=0;i<=imgArray.length;i++){
-    
-    var tr=tbl.insertRow();
-    var td=tr.insertCell();
-    
-    if (i<4){
-        img=document.createElement("img");
-        img.setAttribute("src",imgArray[i]);
-        img.setAttribute("id","bannerImg"+i);
-        img.addEventListener("click",showDescript);
-        img.style.width="200px";
+for (var i = 0; i <= imgArray.length; i++) {
+
+    var tr = tbl.insertRow();
+    var td = tr.insertCell();
+
+    if (i < imgArray.length) {
+        img = document.createElement("img");
+        img.setAttribute("src", imgArray[i]);
+        img.setAttribute("id", "bannerImg" + i);
+        img.addEventListener("click", showDescript);
+        img.style.width = "200px";
         td.appendChild(img);
     }
-    if(i==0){
-        var td=tr.insertCell();
-        td.setAttribute('rowSpan',imgArray.length);
-        td.setAttribute("id","description");
+    if (i == 0) {
+        var td = tr.insertCell();
+        td.setAttribute('rowSpan', imgArray.length);
+        td.setAttribute("id", "description");
         td.appendChild(document.createTextNode("Click One Image For Description"));
-        td.style.fontSize="40px";
+        td.style.fontSize = "40px";
     }
-    
+
 }
 position.appendChild(tbl);
-function showDescript (){
-   var currentImage= event.currentTarget;
-   var id=currentImage.getAttribute("id").substring(9);
-   
-    var td=document.getElementById("description");
-    td.innerHTML=imgDescri[id];
-  
+function showDescript() {
+    var currentImage = event.currentTarget;
+    var id = currentImage.getAttribute("id").substring(9);
+
+    // var td = document.getElementById("description");
+
+    //get content from particular table positon
+    tbl.rows[1].cells[1].innerHTML = imgDescri[id];
+
 }
 
 
--- register-script.js
+++ register-script.js
@@ -1,20 +1,48 @@
+var myForm = document.registerForm;
+
+//show insturction for customerName input
+focusBlur(myForm, "customerName", "namePopupText");
+
+//show instruction for address input
+focusBlur(myForm, "customerAddress", "addressPopupText");
+
+//show instruction for city input
+focusBlur(myForm, "customerCity", "cityPopupText");
+
+//Province
+focusBlur(myForm, "customerProvince", "provincePopupText");
+
+//Postal Code
+focusBlur(myForm, "customerPostalCode", "postalCodePopupText");
+
+//show/hide insturction for different input
+function focusBlur(myForm, elementName, popupId) {
+    var tempElementName = myForm[elementName];
+    tempElementName.addEventListener("focus", function (event) {
+        var popup = document.getElementById(popupId);
+        popup.classList.toggle("show");
+    });
+    tempElementName.addEventListener("blur", function (event) {
+        var popup = document.getElementById(popupId);
+        popup.classList.toggle("show");
+    });
+}
+
 var formRest = document.getElementById("formRest");
 formRest.addEventListener("click", function (event) {
     event.preventDefault();
- 
-
-    // var answer=confirm("Are you sure to reset the form");
-    // if (answer) {
-    //     console.log("user want to reset");
-    // }
+    var answer = confirm("Are you sure to Reset the from");
+    if (answer) {
+        myForm.reset();
+    }
 })
-formRest.addEventListener("mouseover",function(event){
-    var popup = document.getElementById("popupText");
+formRest.addEventListener("mouseover", function (event) {
+    var popup = document.getElementById("resetPopupText");
     popup.classList.toggle("show");
     console.log(popup);
 })
-formRest.addEventListener("mouseout",function(event){
-    var popup = document.getElementById("popupText");
+formRest.addEventListener("mouseout", function (event) {
+    var popup = document.getElementById("resetPopupText");
     popup.classList.toggle("show");
     console.log(popup);
 })
@@ -22,8 +50,11 @@ formRest.addEventListener("mouseout",function(event){
 var formSubmit = document.getElementById("formSubmit");
 formSubmit.addEventListener("click", function (event) {
     event.preventDefault();
-    var answer = confirm("Are you sure to submit the from");
-    if (answer) {
-        console.log("user want to submit");
+    var regExPostalCode=/^[A-Z]\d[A-Z] ?\d[A-Z]\d$/;
+    if (regExPostalCode.test(myForm.customerPostalCode.value)){
+        myForm.submit();
+    }
+    else{
+        alert("Please input a correct Potal Cde in Format:T3K1H1");
     }
 })
